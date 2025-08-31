bb044d29f5ea247bc35de83d0df9d810391c4e70
Girivardhan34 <jhangir2908@gmail.com>
2025-08-31 18:47:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Button from './components/Button';
-import Show from './components/Show';
-import React, { useState } from 'react';
+import Button from "./components/Button";
+import Show from "./components/Show";
+import React, { useState } from "react";
 import * as math from "mathjs";
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
   const [result, setResult] = useState("");
 
   const addText = (val) => {
-    setText((prev) => prev + val); 
+    setText((prev) => prev + val);
   };
 
   const calculate = () => {
@@ -27,44 +27,42 @@ const App = () => {
   };
 
   return (
-    <>
-
-    <div className='App'>
-      <div className='wrap'>
-        <Show text={text} result={result} />
-
-        <div className="row1">
-          <Button symbol="7" handleclick={addText} color="#323232cc" />
-          <Button symbol="8" handleclick={addText} color="#323232cc"/>
-          <Button symbol="9" handleclick={addText} color="#323232cc" />
-          <Button symbol="/" handleclick={addText} color="#f2a33c" />
+    <div className="container d-flex justify-content-center align-items-center min-vh-100" style={{borderRadius:"50px"}}>
+      <div className="bg-white text-light p-3 p-md-4 rounded-2" style={{ maxWidth: "400px", width: "100%" }}>
+        
+  
+        <div className="mb-3">
+          <Show text={text} result={result} />
         </div>
 
-        <div className="row1">
-          <Button symbol="4" handleclick={addText} color="#323232cc"/>
-          <Button symbol="5" handleclick={addText} color="#323232cc"/>
-          <Button symbol="6" handleclick={addText} color="#323232cc"/>
-          <Button symbol="*" handleclick={addText} color="#f2a33c" />
-        </div>
 
-        <div className="row1">
-          <Button symbol="1" handleclick={addText} color="#323232cc"/>
-          <Button symbol="2" handleclick={addText} color="#323232cc"/>
-          <Button symbol="3" handleclick={addText} color="#323232cc"/>
-          <Button symbol="+" handleclick={addText} color="#f2a33c" />
-        </div>
+        <div className="row g-2">
+          <div className="col-3"><Button symbol="7" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="8" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="9" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="/" handleclick={addText} color="#f2a33c" /></div>
 
-        <div className="row1">
-          <Button symbol="0" handleclick={addText} color="#323232cc"/>
-          <Button symbol="." handleclick={addText} color="#323232cc"/>
-          <Button symbol="=" handleclick={calculate} color="#323232cc"/>
-          <Button symbol="-" handleclick={addText} color="#f2a33c" />
-        </div>
+          <div className="col-3"><Button symbol="4" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="5" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="6" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="*" handleclick={addText} color="#f2a33c" /></div>
 
-        <Button symbol="clear" handleclick={reset} color="red" />
+          <div className="col-3"><Button symbol="1" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="2" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="3" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="+" handleclick={addText} color="#f2a33c" /></div>
+
+          <div className="col-3"><Button symbol="0" handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="." handleclick={addText} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="=" handleclick={calculate} color="#323232cc" /></div>
+          <div className="col-3"><Button symbol="-" handleclick={addText} color="#f2a33c" /></div>
+
+          <div className="col-12">
+            <Button symbol="clear" handleclick={reset} color="red" />
+          </div>
+        </div>
       </div>
     </div>
-    </>
   );
 };
 
